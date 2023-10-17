3777f9df0aefa263357f4cae4c006ce1eaf0d532
lucasxs21 <lucasfranco147@gmail.com>
2023-10-17 15:31:28 -0300

    .

--- PryFrancoIE1/ClsvalidacionID.cs
+++ PryFrancoIE1/ClsvalidacionID.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Data.OleDb;
 
 namespace PryFrancoIE1
 {
@@ -12,10 +13,51 @@ namespace PryFrancoIE1
         private string usuarioValido = "Admin"; // Define el nombre de usuario válido
         private string contraseñaValida = "Admin"; // Define la contraseña válida
 
+
+
         public bool ValidarUsuario(string username, string password)
         {
-            return username == usuarioValido && password == contraseñaValida;
+            
+
+            // Cadena de conexión 
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=SistemaBroker.accdb;";
+
+            // Crear una conexión 
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Consulta SQL para seleccionar usuarios y contraseñas de la tabla "User"
+                    string query = "SELECT usuario, contraseña FROM User";
+
+                    // Crear un comando SQL
+                    using (OleDbCommand command = new OleDbCommand(query, connection))
+                    {
+                        // Ejecutar la consulta y obtener un lector de datos
+                        using (OleDbDataReader reader = command.ExecuteReader())
+                        {
+                            // Iterar a través de los registros y mostrar usuarios y contraseñas
+                            while (reader.Read())
+                            {
+                                string usuario = reader["usuario"].ToString();
+                                string contraseña = reader["contraseña"].ToString();
+
+                                return username == usuario && password == contraseña;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+
+            }
         }
         public static string Usuario { get; set; }
+
+
     }
 }
